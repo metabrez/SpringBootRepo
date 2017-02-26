8b35c3eeca1c1d1fa4a301e697cf196a0b900e6b
Tabrez Alam <metabrez@gmail.com>
2017-02-26 12:17:01 -0600

    second comit

--- src/main/java/com/example/SpringGitApplication.java
+++ src/main/java/com/example/SpringGitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringGitApplication {
 
 	public static void main(String[] args) {
+		//run program
 		SpringApplication.run(SpringGitApplication.class, args);
 	}
 }
